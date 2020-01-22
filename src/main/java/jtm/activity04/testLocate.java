f23cf93d808f30fde17797e37337e3ad367dcbe4
belecka.irina@gmail.com <belecka.irina@gmail.com>
2020-01-22 20:47:02 +0200

    +fundamentals

--- src/main/java/jtm/activity04/testLocate.java
+++ src/main/java/jtm/activity04/testLocate.java
@@ -1,16 +1,40 @@
 package jtm.activity04;
 
-import java.util.Locale;
-
 public class testLocate {
 
 	public static void main(String[] args) {
-//		int num =3;
-		float num1 = 2.2211f;
-		System.out.println(num1);
-//		System.out.println(String.format(Locale.US, "%.2f", num) );
-		System.out.println(String.format(Locale.US, "%.3f", num1) );
+		int[] arr = { 1, 2, 3, 4, 3, 2, 1 };
+		int a = findEvenIndex(arr);
+		System.out.println(a);
 
 	}
 
-}
+	public static int findEvenIndex(int[] arr) {
+	int sum1 = 0;
+	int sum2 = 0;
+	int i;
+	int j;
+	for (j = 0; j<arr.length; j++)
+	{	
+		for ( i = 0; i < j; i++) {
+			sum1 += arr[i];
+		}
+		System.out.println("sum1 = " + sum1);
+				
+		for (i = j+1; i < arr.length; i++) {
+			sum2 += arr[i];
+	  
+	    }
+		System.out.println("sum2 = " + sum2);
+		if (sum1==sum2) {
+			return j;
+		} else {
+			sum1 = 0;
+			sum2 = 0;
+		}
+	}
+
+	return -1;
+	}
+
+	}
--- src/main/java/jtm/tasks/fundamentals/EqualSidesArray.java
+++ src/main/java/jtm/tasks/fundamentals/EqualSidesArray.java
@@ -22,7 +22,31 @@ public class EqualSidesArray {
      */
 
     public static int findEvenIndex(int[] arr) {
-        return -1;
-    }
+    	int sum1 = 0;
+    	int sum2 = 0;
+    	int i;
+    	int j;
+    	for (j = 0; j<arr.length; j++)
+    	{	
+    		for ( i = 0; i < j; i++) {
+    			sum1 += arr[i];
+    		}
+    		System.out.println("sum1 = " + sum1);
+    				
+    		for (i = j+1; i < arr.length; i++) {
+    			sum2 += arr[i];
+    	  
+    	    }
+    		System.out.println("sum2 = " + sum2);
+    		if (sum1==sum2) {
+    			return j;
+    		} else {
+    			sum1 = 0;
+    			sum2 = 0;
+    		}
+    	}
+
+    	return -1;
+    	}
 
 }
